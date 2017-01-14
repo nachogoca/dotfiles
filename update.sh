a970da5621508424025ba901b19e9bc5da899098
nachogoca <nacho.gon.car@gmail.com>
2017-01-14 00:36:30 -0600

    Added urxvt config file. Added battery popup. Added update.sh

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script is intended to update config files for the following applications
+# i3
+# i3 lock
+# i3 status bar
+# urxvt
+
+cp ~/.config/i3/* ~/workspace/config/i3
+cp ~/.config/i3lock/* ~/workspace/config/i3lock
+cp ~/.config/i3status/* ~/workspace/config/i3status
+cp ~/.Xresources ~/workspace/config/
+
