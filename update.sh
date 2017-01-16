5752c12f077098dd161a56c8dfdc2f2b3b7fe2a6
nachogoca <nacho.gon.car@gmail.com>
2017-01-15 18:38:59 -0600

    Remap caps key for esc and ctr.

--- update.sh
+++ update.sh
@@ -10,4 +10,4 @@ cp ~/.config/i3/* ~/workspace/config/i3
 cp ~/.config/i3lock/* ~/workspace/config/i3lock
 cp ~/.config/i3status/* ~/workspace/config/i3status
 cp ~/.Xresources ~/workspace/config/
-
+cp ~/.profile ~/workspace/config/
